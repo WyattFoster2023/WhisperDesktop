59b5d67dd0550ec35e32811c959d8967082b3859
WyattFoster2023 <wyattfosteredu@gmail.com>
2025-05-29 20:19:43 -0400

    feat(recorder): Implement start_recording and stop_recording methods for Recorder

--- src/recorder/recorder.py
+++ src/recorder/recorder.py
@@ -30,3 +30,44 @@ class Recorder:
         self._wave_file = None
 
     # Implementation of methods will follow in subsequent subtasks. 
+
+    def start_recording(self, mode=RecordingMode.TOGGLE):
+        if self._recording:
+            return
+        self._mode = mode
+        self._recording = True
+        os.makedirs('recordings', exist_ok=True)
+        timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        self._file_path = f'recordings/recording_{timestamp}.wav'
+        self._wave_file = wave.open(self._file_path, 'wb')
+        self._wave_file.setnchannels(self._channels)
+        self._wave_file.setsampwidth(self._audio.get_sample_size(self._format))
+        self._wave_file.setframerate(self._sample_rate)
+        self._stream = self._audio.open(
+            format=self._format,
+            channels=self._channels,
+            rate=self._sample_rate,
+            input=True,
+            frames_per_buffer=self._chunk_size,
+            stream_callback=self._audio_callback
+        )
+        self._event_bus.publish(EventType.RECORDING_STARTED, self._file_path)
+
+    def _audio_callback(self, in_data, frame_count, time_info, status):
+        self._wave_file.writeframes(in_data)
+        return (in_data, pyaudio.paContinue)
+
+    def stop_recording(self):
+        if not self._recording:
+            return
+        self._recording = False
+        if self._stream:
+            self._stream.stop_stream()
+            self._stream.close()
+            self._stream = None
+        if self._wave_file:
+            self._wave_file.close()
+            self._wave_file = None
+        self._event_bus.get_queue('transcription').put(self._file_path)
+        self._event_bus.publish(EventType.RECORDING_STOPPED, self._file_path)
+        return self._file_path 
